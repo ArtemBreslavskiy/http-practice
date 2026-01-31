5f1d1573fb5eeef50fd7698497b17042061048aa
211e3414e21 <rqqerewgtsdfhbd>
2026-01-31 14:14:46 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
