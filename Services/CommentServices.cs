1a26484872bb67ff26ed00b649745c2d64c32e0b
211e3414e21 <rqqerewgtsdfhbd>
2026-02-01 14:25:58 +0500

    add create comment

--- Services/CommentServices.cs
+++ Services/CommentServices.cs
@@ -1,26 +1,45 @@
-﻿using http_practice.Dto;
+﻿using ConsoleApp21.Dto;
+using http_practice.Dto;
 using http_practice.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApp21.Services
+namespace http_practice.Services
 {
-    internal class CommentServices
+    internal class UserServices
     {
         private readonly HttpClient _http;
 
-        public CommentServices()
+        public UserServices()
         {
             _http = ApiClient.Instance;
         }
 
-        public async Task<List<PostDto>> GetAllAsync()
+        public async Task<List<CommentDto>> GetAllAsync()
         {
             try
             {
-                return await _http.GetFromJsonAsync<List<PostDto>>("/api/posts");
+                return await _http.GetFromJsonAsync<List<CommentDto>>("/api/comments");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error HTTP request: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error get all users: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponce> Create(CommentDto comment)
+        {
+            try
+            {
+                return await _http.PostAsJsonAsync(comment);
             }
             catch (HttpRequestException ex)
             {
